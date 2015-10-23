378ec1609789e01b1b8d39ecfb0f476b1b7f54bf
Juan David <juandominguez2017@u.northwestern.edu>
2015-10-23 01:26:16 -0500

    Pseudocode to implement hough circle recognition.

--- irisDetectionHelper.py
+++ irisDetectionHelper.py
@@ -23,3 +23,46 @@ def processAcc(emptyAccumulator, binaryEdgeImg, edges):
             for edge in edges:
                 radius = int(np.linalg.norm(np.array([r, c]) - np.array(edge)))
                 emptyAccumulator[r][c][radius] += 1
+                
+                
+"""
+def makeCircles():
+def getCircles(maxR):
+    circles = None
+    fileNameCircles, fileMaxR = 
+    if circles not saved: 
+        circles = makeCircles(maxR)
+        pickle.save(circles)
+    else:
+        circles = pickle.load(circles)
+    return circles
+
+def removeBorders(img, maxR):
+    rows, cols = img.shape()
+    zeroedMatrix = numpy.zeros((rows, cols), dtype = same as image)
+    radiusVector = [sin(45)maxR, cos(45)maxR]
+    upperLeftCorner = [0,0] + radiusVector
+    lowerRightCorner = [rows, cols] - radiusVector
+    roi = regionOfInterest where x goes from 
+        upperLeftCorner[0] to lowerRightCorner[0], 
+        and y goes from upperLeftCorner[1] to lowerRightCorner[1]
+    return zeroedMatrix + roi
+
+def prepareImg(fileName, maxR):
+    img = blackWhiteImage
+    edgeImg = cannyEdgeDetect
+    return removeBorders(img, maxR)
+    
+    
+def houghCircle(img, minR, maxR, edgesArr, circles, acc):
+    for center in edgesArr:
+        for r in xrange(minR:maxR):
+            acc += circleMatrix(circles, r, center)
+        
+    return getMaxCenterR(acc)
+"""
+
+
+
+
+
