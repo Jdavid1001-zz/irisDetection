b68d3b69c189e2b31a0aa583b8fca00623878285
Juan David <juandominguez2017@u.northwestern.edu>
2015-10-27 01:42:05 -0500

    Makes irisDetection.py mirror exp.py

--- irisDetection.py
+++ irisDetection.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#import irisDetectionHelper as idh
+import irisDetectionHelper as idh
 import cv2
 import cv2.cv as cv
 import numpy as np
@@ -8,31 +8,39 @@ import os
 
 filename = '/Users/JuanDa/Documents/Academics/2015-2016/Fall/EECS\ 395\ Biometrics/hw2/2008-03-11_13/04233/04233d1712.tiff'
 
-def process(path):
-    img = cv2.imread(path,0)
-    img = cv2.medianBlur(img,5)
-    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-    circles = cv2.HoughCircles(cimg,cv.CV_HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=30,maxRadius=150)
+def getIris(img):
+    iris = idh.findIris(img)
+    roiForPupil, squareRowCol = idh.fromCircGetROI(img, iris)
+    pupil = idh.findPupil(roiForPupil)
+    return pupil, iris
 
-    circles = np.around(circles)
+def drawIris(img, pupil, iris):
+    cImg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    cImg = idh.drawCircle(cImg, iris)
+    cImg = idh.drawCircle(cImg, pupil)
     
-    for i in circles[0,:]:
-        # draw the outer circle
-        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-        # draw the center of the circle
-        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+def getFileName(outFolder, fileNum):
+    return 'iris_' + str(fileNum) + '.jpg'
     
-    cv2.imshow('detected circles',cimg)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def saveImage(img, outFolder, fileNum):
+    fileName = getFileName(outFolder, fileNum)
+    
+
+def process(path, outFolder):
+    img = cv2.imread(filename, 0)
+    irisInner, irisOuter = getIris(img)
+    coloredIrisImg = drawIris(img, irisInner, irisOuter)
+    saveImage(coloredIrisImg, outFolder)
 
 process(filename)
 
 
 """
 folder = 'C:/Users/Joel/Desktop/2008-03-11_13/2008-03-11_13/'
+currentFile = 0
 for nextfolder in os.listdir(folder):
     for f in os.listdir(folder + nextfolder)[1:]:
-        process(folder+nextfolder+'/'+f)
+        process(folder+nextfolder+'/'+f, currentFile)
+        currentFile += 1
         
 """
